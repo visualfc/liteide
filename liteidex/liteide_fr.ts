37decdd47ecccabc31ec0860c34c55e3bbd080fa
Dobrosław Żybort <matrixik@gmail.com>
2012-12-06 11:03:20 +0100

    Update license dates

--- liteidex/liteide_fr.ts
+++ liteidex/liteide_fr.ts
@@ -118,7 +118,7 @@ This library is free software; you can redistribute it and/or modify it under th
 This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 
 In addition, as a special exception, that plugins developed for LiteIDE, are allowed to remain closed sourced and can be distributed under any license .These rights are included in the file LGPL_EXCEPTION.txt in this package.</source>
-        <oldsource>Copyright (c) 2011 LiteIDE team. All rights reserved.
+        <oldsource>Copyright (c) 2011-2012 LiteIDE team. All rights reserved.
 
 This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
 
