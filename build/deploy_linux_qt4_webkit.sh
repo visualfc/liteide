4edd235c9937eb6cff11f4c9b4d885d1bda10915
visualfc <visualfc@gmail.com>
2013-05-13 10:28:55 +0800

    update linux/build qt4

---
+++ build/deploy_linux_qt4_webkit.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+export BUILD_ROOT=$PWD
+
+if [ -z $LITEIDE_ROOT ]; then
+	export LITEIDE_ROOT=$PWD/../liteidex
+fi
+
+echo build liteide 
+echo QTDIR=$QTDIR
+echo GOROOT=$GOROOT
+echo BUILD_ROOT=$BUILD_ROOT
+echo LITEIDE_ROOT=$LITEIDE_ROOT
+echo .
+
+if [ -z $QTDIR ]; then
+	echo 'error, QTDIR is null'
+	exit 1
+fi
+
+cp -a -v $QTDIR/lib/libQtCore.so* liteide/lib/liteide
+cp -a -v $QTDIR/lib/libQtGui.so* liteide/lib/liteide
+cp -a -v $QTDIR/lib/libQtXml.so* liteide/lib/liteide
+
+cp -a -v $QTDIR/lib/libphonon.so* liteide/lib/liteide
+cp -a -v $QTDIR/lib/libQtNetwork.so* liteide/lib/liteide
+cp -a -v $QTDIR/lib/libQtWebKit.so* liteide/lib/liteide
+cp -a -v $QTDIR/lib/libQtDBus.so* liteide/lib/liteide
