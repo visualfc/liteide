5863feb4caefbbded7c0fab0bfc8a9e136943cb1
visualfc <visualfc@gmail.com>
2013-05-13 09:55:05 +0800

    add build/deploy qt4 qt5

---
+++ build/deploy_qt4.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal
+
+set BUILD_ROOT=%CD%
+if x%LITEIDE_ROOT%==x set LITEIDE_ROOT=%CD%\..\liteidex
+
+echo build liteide 
+echo QTDIR=%QTDIR%
+echo .
+
+if x%QTDIR%==x goto qtdir_fail
+
+xcopy %QTDIR%\bin\Qt5Core.dll liteide\bin /y
+xcopy %QTDIR%\bin\Qt5Gui.dll liteide\bin /y 
+xcopy %QTDIR%\bin\Qt5Xml.dll liteide\bin /y 
+xcopy %QTDIR%\bin\mingwm10.dll liteide\bin /y
+xcopy %QTDIR%\bin\libgcc_s_dw2-1.dll liteide\bin /y
+
+goto end
+
+:qtdir_fail
+echo error, QTDIR is null
+goto end
+
+:end
