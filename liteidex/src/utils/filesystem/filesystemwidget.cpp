b903da4b89767d0e59f3097c2f2698d243ffffc4
visualfc <visualfc@gmail.com>
2013-07-10 17:10:08 +0800

    update filesystem

--- liteidex/src/utils/filesystem/filesystemwidget.cpp
+++ liteidex/src/utils/filesystem/filesystemwidget.cpp
@@ -64,6 +64,16 @@ FileSystemWidget::FileSystemWidget(LiteApi::IApplication *app, QWidget *parent) 
     m_tree = new SymbolTreeView;
     m_tree->setHeaderHidden(true);
     m_model = new FileSystemModel(this);
+
+    QDir::Filters filters = QDir::AllDirs | QDir::Files | QDir::Drives
+                            | QDir::Readable| QDir::Writable
+                            | QDir::Executable | QDir::Hidden
+                            | QDir::NoDotAndDotDot;
+#ifdef Q_OS_WIN // Symlinked directories can cause file watcher warnings on Win32.
+    filters |= QDir::NoSymLinks;
+#endif
+    m_model->setFilter(filters);
+
     m_tree->setContextMenuPolicy(Qt::CustomContextMenu);
     m_tree->setModel(m_model);
 
