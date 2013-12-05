adc7ac823483307a43aeee5242b70772b2dfced3
visualfc <visualfc@gmail.com>
2013-12-05 20:02:14 +0800

    update sh

--- build/build_linux.sh
+++ build/build_linux.sh
@@ -53,6 +53,8 @@ go install -ldflags "-s" -v tools/goastview
 go install -ldflags "-s" -v tools/godocview
 go install -ldflags "-s" -v tools/goexec
 go install -ldflags "-s" -v tools/goapi
+go install -ldflags "-s" -v tools/gopresent
+go install -ldflags "-s" -v github.com/visualfc/goimports
 
 if [ $? -ge 1 ]; then
 	echo 'error, go install fail'
