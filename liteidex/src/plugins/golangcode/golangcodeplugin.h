c38dd3098476383f56cde5d46d0725eb6b06bab6
visualfc <visualfc@gmail.com>
2015-04-21 15:37:55 +0800

    update ver

--- liteidex/src/plugins/golangcode/golangcodeplugin.h
+++ liteidex/src/plugins/golangcode/golangcodeplugin.h
@@ -58,7 +58,7 @@ public:
         m_info->appendDepend("plugin/golangast");
         m_info->setName("GolangCode");
         m_info->setAuthor("visualfc");
-        m_info->setVer("X27");
+        m_info->setVer("X27.2");
         m_info->setInfo("Golang Gocode Support");
     }
 };
--- liteidex/src/plugins/liteeditor/liteeditorplugin.h
+++ liteidex/src/plugins/liteeditor/liteeditorplugin.h
@@ -47,7 +47,7 @@ public:
         m_info->setId("plugin/liteeditor");
         m_info->setName("LiteEditor");
         m_info->setAuthor("visualfc");
-        m_info->setVer("X27.1");
+        m_info->setVer("X27.2");
         m_info->setInfo("Core Editor");
         m_info->appendDepend("plugin/litebuild");
         m_info->setMustLoad(true);
