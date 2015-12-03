d25927923c28997ce357615ecd050f764040effb
visualfc <visualfc@gmail.com>
2015-12-03 16:57:54 +0800

    astview support go1.5 vendor

--- liteidex/src/plugins/golangast/golangast.cpp
+++ liteidex/src/plugins/golangast/golangast.cpp
@@ -25,6 +25,7 @@
 #include "golangastitem.h"
 #include "golangasticon.h"
 #include "astwidget.h"
+#include "liteenvapi/liteenvapi.h"
 
 #include <QStackedWidget>
 #include <QDockWidget>
@@ -334,6 +335,7 @@ void GolangAst::updateAstNow()
     QStringList args;
     args << "astview";
     args << m_updateFileNames;
+    m_process->setEnvironment(LiteApi::getGoEnvironment(m_liteApp).toStringList());
     m_process->start(cmd,args);
 }
 
@@ -354,6 +356,7 @@ void GolangAst::updateAstNowFile()
     QStringList args;
     args << "astview";
     args << m_editorFileName;
+    m_processFile->setEnvironment(LiteApi::getGoEnvironment(m_liteApp).toStringList());
     m_processFile->start(cmd,args);
 }
 
