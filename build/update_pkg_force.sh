06c81fb531ffcf42cb9dff1b1b840cc84018c5e8
visualfc <visualfc@gmail.com>
2017-06-12 20:21:53 +0800

    add update_pkg_force script

---
+++ build/update_pkg_force.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+export BUILD_ROOT=$PWD
+
+if [ -z $LITEIDE_ROOT ]; then
+	export LITEIDE_ROOT=$PWD/../liteidex
+fi
+
+echo GOROOT=$GOROOT
+echo BUILD_ROOT=$BUILD_ROOT
+echo LITEIDE_ROOT=$LITEIDE_ROOT
+echo .
+
+go version
+if [ $? -ge 1 ]; then
+	echo 'error, not find go in PATH'
+	exit 1
+fi
+
+echo update liteide tools ...
+cd $LITEIDE_ROOT
+
+if [ -z $GOPATH ]; then
+	export GOPATH=$PWD
+else
+	export GOPATH=$PWD:$GOPATH
+fi
+
+echo get gocode ...
+rm -f -r src/github.com/nsf/gocode
+go get -v -u -ldflags "-s" "github.com/nsf/gocode"
+echo get gotools ...
+rm -f -r src/github.com/visualfc/gotools
+go get -v -u -ldflags "-s" "github.com/visualfc/gotools"
+
+if [ $? -ge 1 ]; then
+	echo 'error, go install fail'
+	exit 1
+fi
+
+cd $BUILD_ROOT
