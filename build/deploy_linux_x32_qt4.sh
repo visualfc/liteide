13e6360642fe6679a793603a7ee628eec0c7cfc5
visualfc <visualfc@gmail.com>
2018-02-18 07:50:03 +0800

    add deploy linux x32

---
+++ build/deploy_linux_x32_qt4.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+export BUILD_ROOT=$PWD
+
+if [ -z $LITEIDE_ROOT ]; then
+	export LITEIDE_ROOT=$PWD/../liteidex
+fi
+
+QT4LIBS=/usr/lib/i386-linux-gnu
+echo "Setting QT4LIBS=$QT4LIBS"
+
+echo build liteide
+echo QT4LIBS=$QT4LIBS
+echo GOROOT=$GOROOT
+echo BUILD_ROOT=$BUILD_ROOT
+echo LITEIDE_ROOT=$LITEIDE_ROOT
+echo .
+
+if [ -z $QT4LIBS ]; then
+	echo 'error, QT4LIBS is null'
+	exit 1
+fi
+
+cp -a -v $QT4LIBS/libQtCore.so* liteide/lib/liteide
+cp -a -v $QT4LIBS/libQtGui.so* liteide/lib/liteide
+cp -a -v $QT4LIBS/libQtXml.so* liteide/lib/liteide
