dd160b1d0b57620e45c6102d6a0f5d2ad3e4d0ed
visualfc <visualfc@gmail.com>
2018-03-16 07:16:41 +0800

    add deploy linux qt5.5

---
+++ build/deploy_linux_x32_qt5.5.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export BUILD_ROOT=$PWD
+
+if [ -z $LITEIDE_ROOT ]; then
+	export LITEIDE_ROOT=$PWD/../liteidex
+fi
+
+QT5LIBS=/usr/lib/i386-linux-gnu
+echo "Setting QT5LIBS=$QT5LIBS"
+
+echo build liteide
+echo QT5LIBS=$QT5LIBS
+echo GOROOT=$GOROOT
+echo BUILD_ROOT=$BUILD_ROOT
+echo LITEIDE_ROOT=$LITEIDE_ROOT
+echo .
+
+if [ -z $QT5LIBS ]; then
+	echo 'error, QT5LIBS is null'
+	exit 1
+fi
+
+cp -a -v $QT5LIBS/libQt5Core.so* liteide/lib/liteide
+cp -a -v $QT5LIBS/libQt5Gui.so* liteide/lib/liteide
+cp -a -v $QT5LIBS/libQt5Widgets.so* liteide/lib/liteide
+cp -a -v $QT5LIBS/libQt5Xml.so* liteide/lib/liteide
+cp -a -v $QT5LIBS/libicudata.so* liteide/lib/liteide
+cp -a -v $QT5LIBS/libicui18n.so* liteide/lib/liteide
+cp -a -v $QT5LIBS/libicuuc.so* liteide/lib/liteide
