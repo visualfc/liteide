eb2a4c91594b19c1a47bd5cb3a6a18cff9938337
visualfc <visualfc@gmail.com>
2018-07-09 08:25:39 +0800

    x

---
+++ build/deploy_windows_qt5.9.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+setlocal
+
+set BUILD_ROOT=%CD%
+if x%LITEIDE_ROOT%==x set LITEIDE_ROOT=%CD%\..\liteidex
+
+echo build liteide 
+echo QTDIR=%QTDIR%
+echo .
+
+if x%QTDIR%==x goto qtdir_fail
+
+windeployqt --no-angle --no-opengl-sw --dir liteide/bin liteide/lib/liteide/plugins/liteeditor.dll
+
+windeployqt --no-angle --no-opengl-sw --dir liteide/bin liteide/lib/liteide/plugins/welcome.dll
+
+windeployqt --no-angle --no-opengl-sw --dir liteide/bin liteide/lib/liteide/plugins/litebuild.dll
+
+windeployqt --no-angle --no-opengl-sw --dir liteide/bin liteide/lib/liteide/plugins/dlvdebugger.dll
+
+windeployqt --no-angle --no-opengl-sw --dir liteide/bin liteide/bin/liteapp.dll
+
+
+
+goto end
+
+:qtdir_fail
+echo error, QTDIR is null
+goto end
+
+:end
