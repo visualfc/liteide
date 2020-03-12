35b1c7f0ad328a54d850078351ac336ec69fa243
visualfc <visualfc@gmail.com>
2020-03-13 07:06:44 +0800

    litefind: SearchResultWidget repaint

--- liteidex/src/3rdparty/qtc_searchresult/searchresultwidget.cpp
+++ liteidex/src/3rdparty/qtc_searchresult/searchresultwidget.cpp
@@ -548,7 +548,7 @@ void SearchResultWidget::showReplaceMode()
         }
     }
     this->m_replaceTextEdit->setFocus();
-    this->layout()->update();
+    this->m_searchResultTreeView->repaint();
 }
 
 QList<SearchResultItem> SearchResultWidget::checkedItems() const
