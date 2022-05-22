e47d2b8222db9244178fb7129b06c9fd209d2e30
visualfc <visualfc@gmail.com>
2022-05-22 21:00:48 +0800

    build: update build windows 386 for mingw

--- build/build_windows_386_mingw32.cmd
+++ build/build_windows_386_mingw32.cmd
@@ -1,5 +1,6 @@
 set GOARCH=386
 call build_windows_mingw32.cmd
+call update_pkg.cmd
 
 if x%LITEIDE_ROOT%==x set LITEIDE_ROOT=%CD%\..\liteidex
 xcopy %LITEIDE_ROOT%\bin\windows_386\gotools.exe liteide\bin /y
